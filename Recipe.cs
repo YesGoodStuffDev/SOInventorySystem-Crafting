11619ad1c8c45b1e147a117d5b29a1cab362410b
Pat <93166642+YesGoodStuffDev@users.noreply.github.com>
2023-08-12 19:47:46 +1000

    Update Recipe.cs

--- Recipe.cs
+++ Recipe.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Recipe", menuName = "InventorySystem/Recipes/Recipe")]
 public class Recipe : ScriptableObject
 {
+    //Add multiple items in the list if the crafting materials requires multiple of the same item
     public List<ItemObject> ingredients;
     public ItemObject outcome;
 }
